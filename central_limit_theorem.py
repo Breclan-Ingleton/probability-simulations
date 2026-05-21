748ce5b379f939b60842d539d2639bd6ca368cd9
Breclan Ingleton <breclan.ingleton@icloud.com>
2026-05-21 11:44:46 +0100

    Add central limit theorem simulation

---
+++ central_limit_theorem.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+num_samples = 10000
+sample_size = 30
+
+samples = np.random.randint(1, 7, size=(num_samples, sample_size))
+sample_means = samples.mean(axis=1)
+
+plt.hist(sample_means, bins=40)
+
+plt.title("Central Limit Theorem: Dice Roll Sample Means")
+plt.xlabel("Sample mean")
+plt.ylabel("Frequency")
+
+plt.show()
